3976783b87b7321f8a7384e13badc9e27785adb9
Denise Pendleton <dpendl1@hotmail.com>
2018-04-05 19:00:18 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ end
 def can_be_created_in_a_block(args = {})
   Movie.create do |m|
   #   m.title = array[0]
-  m = args.each {|key, value| Movie.send(("#{key}="), value)}
+  m = args.each {|key, value| Movie.send(("#{key}:"), value)}
   #  Movie.create(args) if !args.empty?
   end
 end
