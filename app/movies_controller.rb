c3971cd9077bfc6969081df587a39196d808a175
Denise Pendleton <dpendl1@hotmail.com>
2018-04-05 19:08:09 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ end
 def can_be_created_in_a_block(args = {title: "Home Alone"})
   Movie.create do |m|
   #   m.title = array[0]
-  m.title = "Home ALone"
+  m.title = "Home Alone"
   #  Movie.create(args) if !args.empty?
   end
 end
