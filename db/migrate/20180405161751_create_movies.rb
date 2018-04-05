3b154e76c44995605138e51ff6b3f424e0a58e51
Denise Pendleton <dpendl1@hotmail.com>
2018-04-05 16:20:20 +0000

    automatically backed up by learn

--- db/migrate/20180405161751_create_movies.rb
+++ db/migrate/20180405161751_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table movies do |t|
+      t.string :title
+      t.datetime :release_date
+      t.string  :director
+      t.string :lead,
+      t.bolean :in_theaters
   end
 end
